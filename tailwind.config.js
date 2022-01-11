fdf81fec0df756d0b0b12fbe3847f57f8c1cdd73
岩崎拓海 <>
2022-01-11 22:02:24 +0900

    added config files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
