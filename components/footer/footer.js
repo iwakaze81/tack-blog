1d60cda6ae6da29e2c2e13d0e391e94ea021d858
岩崎拓海 <>
2022-01-18 23:39:14 +0900

    Added kusa on footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,11 +3,8 @@ import { BsGithub, BsTwitter } from "react-icons/bs"
 
 export function Footer() {
   return (
-    <div className="bottom-0 w-full h-40 pb-10 flex flex-col justify-between bg-gray-800">
+    <div className="bottom-0 w-full h-80 2xl:pb-24 flex flex-col bg-gray-800">
       <div className="p-4 text-white">
-        <div>
-          <span></span>
-        </div>
         <div className="m-2 flex items-center">
           <Image 
             src="https://images.microcms-assets.io/assets/c7f638adc3f64d9192bd4907efc2835a/8364714ee36d49e8a119d8489d494080/IMG_98C696D2B7E0-1.png"
@@ -31,6 +28,20 @@ export function Footer() {
         </div>
       </div>
       <div className="text-right mr-4">
+        <a href="https://github.com/iwakaze81">
+          <div className="flex h-28 ml-12 mb-4 bg-image">
+            <style jsx>
+              {`
+                .bg-image {
+                  background: left;
+                  background-size: contain;
+                  background-repeat: no-repeat;
+                  background-image: url('https://grass-graph.appspot.com/images/iwakaze81.png?background=none');
+                }
+              `}
+            </style>
+          </div>
+        </a>
         <p className="copyright text-gray-200">&copy; 2022 TACK</p>
       </div>
     </div>
