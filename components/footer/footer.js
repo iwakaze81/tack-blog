6e698d7e5d7f28b5da7b62d9081a093e19b71b9c
岩崎拓海 <>
2022-01-27 21:02:51 +0900

    Added footer title

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,6 +5,7 @@ export function Footer() {
   return (
     <div className="bottom-0 w-full h-80 2xl:pb-24 flex flex-col bg-gray-800">
       <div className="p-4 text-white">
+        <p className="ml-2 font-bold">About Me</p>
         <div className="m-2 flex items-center">
           <Image 
             src="https://images.microcms-assets.io/assets/c7f638adc3f64d9192bd4907efc2835a/8364714ee36d49e8a119d8489d494080/IMG_98C696D2B7E0-1.png"
